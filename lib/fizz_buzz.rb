b64f1b006e4e709396fb54e1a52f16f55f74d963
W.Mochizuki <m1130wata@gmail.com>
2022-07-17 23:11:56 +0900

    first commit

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,19 @@
+def fizz_buzz(number)
+  if number % 15 == 0
+    puts "Fizz Buzz"
+  elsif number % 3 == 0
+    puts "Fizz"
+  elsif number % 5 == 0
+    puts "Buzz"
+  else
+    puts number
+  end
+end
+
+fizz_buzz(1)
+fizz_buzz(2)
+fizz_buzz(3)
+fizz_buzz(4)
+fizz_buzz(5)
+fizz_buzz(6)
+fizz_buzz(15)
